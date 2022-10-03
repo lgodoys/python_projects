b7ab41787cf596e57bec6e2465dc21d436ff8458
lgodoys <leonardo.godoy.2009@gmail.com>
2022-10-02 23:09:49 -0300

    initial commit

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ IPBD = '127.0.0.1'
 NOMBREBD = ''
 BDNOMBRE = 'restaurant'
 USUARIOBD = 'leonardo'
-PASSBD = 'miContrasenaSegura2022'
+PASSBD = 'W4rw1ck.2022365'
 TABLA1 = "empleados"
 TABLA2 = "clientes"
